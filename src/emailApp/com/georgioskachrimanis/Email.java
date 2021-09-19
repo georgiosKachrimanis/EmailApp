798222ed3f81fcc839908cbe0b8be1c1a50e384c
georgioskachrimanis <georgios.kachrimanis@hotmail.com>
2021-09-19 11:39:35 +0200

    1.3 Only switch remains in the main.

--- src/emailApp/com/georgioskachrimanis/Email.java
+++ src/emailApp/com/georgioskachrimanis/Email.java
@@ -1,22 +1,21 @@
 package emailApp.com.georgioskachrimanis;
 
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
 
 public class Email {
 
-    private String firstName;
-    private String lastName;
+    private static final Scanner input = new Scanner(System.in);
+    public static List<Email> usersList = new ArrayList<>();
+
+    private final String firstName;
+    private final String lastName;
     private String password;
-    private String email;
-    private int defaultPasswordLength = 8;
-    private String department;
-    private String companySuffix = "anyCompany.com";
+    private final String email;
+    private final int defaultPasswordLength = 8;
+    private final String department;
     private int mailboxCapacity = 1000;
     private String alternateEmail;
 
-
-    private static Scanner input = new Scanner(System.in);
 
     // Constructors
     public Email(String firstName, String lastName) {
@@ -26,6 +25,7 @@ public class Email {
         this.department = setDepartment();
         System.out.println("Email created for " + lastName + ", " + firstName + " - " + department);
         this.password = setPassword(defaultPasswordLength);
+        String companySuffix = "anyCompany.com";
         this.email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + companySuffix;
         System.out.println("The new email is: " + this.email);
         System.out.println("The new password is: " + password);
@@ -40,6 +40,11 @@ public class Email {
         return lastName;
     }
 
+    public String getEmail() {
+
+        return email;
+    }
+
     public int getDefaultPasswordLength() {
         return defaultPasswordLength;
     }
@@ -52,7 +57,7 @@ public class Email {
         this.mailboxCapacity = mailboxCapacity;
     }
 
-    public void setAlternateEmail(String alternateEmail) {
+    private void setAlternateEmail(String alternateEmail) {
         this.alternateEmail = alternateEmail;
     }
 
@@ -66,8 +71,6 @@ public class Email {
         input.nextLine();
 
         switch (choice) {
-            case 0:
-                return "";
             case 1:
                 return "sales.";
             case 2:
@@ -75,214 +78,195 @@ public class Email {
             case 3:
                 return "accounting.";
         }
-        return null;
+        return "";
     }
 
-    public String setPassword(int length){
+    private String setPassword(int length) {
 
         String passwordItems = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789!@#$%^&*()_+?><";
 
         char[] password = new char[length];
-        for (int i = 0; i < length ; i++) {
-            int randomChar = (int)(Math.random() * passwordItems.length());
+        for (int i = 0; i < length; i++) {
+            int randomChar = (int) (Math.random() * passwordItems.length());
             password[i] = passwordItems.charAt(randomChar);
         }
         return new String(password);
     }
 
-    public String getEmail() {
-        return this.email;
+    public static void printEmail() {
+        Email user = findUser();
+        if (user != null) {
+            System.out.println("The email of " + user.getFirstName() + " "
+                    + user.getLastName() + " is:\n" + user.getEmail());
+        } else {
+            System.out.println("There is no user with this name.");
+        }
     }
+
     public String getPassword() {
         return password;
     }
 
-    // Methods
+    public String getAlternateEmail() {
+        return alternateEmail;
+    }
+    // ----------------------------- Methods ------------------------------------------------
 
-    public static void menuScreen() {
+
+
+
+    public static void optionsMenu() {
         System.out.println("---------------------------------------------------\n"
                 + "Welcome to EmailApp for @genericCompanyName.\n\n\n" +
                 "Available actions:\n\t" +
                 "0. Exit\n\t" +
                 "1. New email account.\n\t" +
                 "2. Get users email address.\n\t" +
-                "3. Get users email password.\n\t" +
-                "4. Password change by the User.\n\t" +
-                "5. Issue new user password.\n\t" +
-                "6. Get users mailbox capacity\n\t" +
-                "7. Change users mailbox capacity.\n\t" +
-                "8. Set alternative email address for user.\n\t" +
-                "9. Print all email accounts.\n" +
-                "Enter your action number: ");
+                "3. Password change by the User.\n\t" +
+                "4. Issue new user password.\n\t" +
+                "5. Get users mailbox capacity\n\t" +
+                "6. Change users mailbox capacity.\n\t" +
+                "7. Set alternative email address for user.\n\t" +
+                "8. Print all email accounts.\n" +
+                "Enter your choise: ");
     }
 
-    // For new password set by the user.
-    public void changePassword(String oldPassword, String newPassword) {
-        if (Objects.equals(this.password, oldPassword)){
-            this.password = newPassword;
-            System.out.println("Password for user: " + firstName + " " + lastName + " is changed.");
+
+    public static void createNewUser() {
+        String firstName = userInput("first name");
+        String lastName = userInput("last name");
+
+        if (findUser(firstName, lastName) == null) {
+            usersList.add(new Email(firstName, lastName));
         } else {
-            System.out.println("The old password you provided is not wrong. Try again or contact your administrator.");
+            System.out.println("The user already exists in the system.\n" +
+                    "Creating new email account is canceled.");
         }
     }
 
+
+    // Change password by the user.
+    public static void userChangePassword() {
+        Email user = findUser();
+        String oldPassword;
+        String newPassword;
+        if (user != null) {
+            System.out.println("Please enter the old password:\n");
+            oldPassword = input.nextLine();
+            System.out.println("Please enter the new password:\n");
+            newPassword = input.nextLine();
+            if (Objects.equals(user.getPassword(), oldPassword)) {
+                user.password = newPassword;
+                System.out.println("Password for user: " + user.getFirstName() + " "
+                        + user.getLastName() + " is changed.");
+            } else {
+                System.out.println("The old password you provided is wrong. Try again or contact your administrator.");
+            }
+        }
+    }
+
+
+    // New password for the user.
+    public static void issueNewPassword() {
+        Email user;
+        user = findUser();
+        if (user != null) {
+            user.setPassword(user.getDefaultPasswordLength());
+            System.out.println(user.getEmail() + " new password is: "
+                    + user.getPassword());
+        } else {
+            System.out.println("There is no user with this name.");
+        }
+    }
+
+    //Print mailbox capacity.
+    public static void mailboxCapacity() {
+       Email user = findUser();
+        if (user != null) {
+            System.out.println(" User with email: " + user.getEmail() +
+                    "\nHas a  mailbox capacity of : " + user.getMailboxCapacity() + " Mb.");
+        } else {
+            System.out.println("There is no user with this name.");
+        }
+    }
+
+
+    public static void alternateMailbox() {
+        Email user = findUser();
+        if (user != null) {
+            System.out.println("Provide me the alternate email of the user: " + user.getEmail());
+            String alternateEmail = input.nextLine();
+            alternateEmail = alternateEmail.replaceAll("\\s+", "");
+            user.setAlternateEmail(alternateEmail);
+            System.out.println("New alternate email is saved.");
+        } else {
+            System.out.println("There is no user with this name.");
+        }
+    }
+
+
+    public static void changeMailboxCapacity() {
+        Email user;
+        user = findUser();
+        if (user != null) {
+            System.out.println("Enter new mailbox capacity for " + user.getEmail());
+            int newCapacity = input.nextInt();
+            input.nextLine();
+            user.setMailboxCapacity(newCapacity);
+        } else {
+            System.out.println("There is no user with this name.");
+        }
+    }
+
+    private static String userInput(String firstOrLastName) {
+        System.out.println("Enter the " + firstOrLastName + " of the user:\r");
+        String name = input.nextLine();
+        // The following line is to eliminate white space and
+        // also to prevent errors from capitalization
+        name = name.replaceAll("\\s+", "").toLowerCase();
+        return name;
+
+    }
+
+
+
+
+    public static void printEmailAccounts() {
+        for (Email email : usersList) {
+            System.out.println("\n------------------------------------------------------------------"+
+                    "User name: " + email.getFirstName() + " "
+                    + email.getLastName() + "\nEmail address: " + email.getEmail()
+                    + "\n------------------------------------------------------------------");
+        }
+    }
+
+
     private static Email findUser() {
-        System.out.println("Enter the first name of the user:\r");
-        String firstName = input.nextLine();
-        System.out.println("Enter the last name of the user:\r");
-        String lastName = input.nextLine();
-
-        // fixing mistypes by users
-        firstName = firstName.replaceAll("\\s+","");
-        lastName = lastName.replaceAll("\\s+", "");
-
-        for (Email checkedUser : EmailApp.usersList) {
+        String firstName = userInput("first name");
+        String lastName = userInput("last name");
+        for (Email checkedUser : usersList) {
             if (checkedUser.getFirstName().equals(firstName)
                     && checkedUser.getLastName().equals(lastName)) {
                 return checkedUser;
             }
+
         }
         return null;
     }
+
 
     // Overloaded method of findUser()
     private static Email findUser(String firstName, String lastName) {
 
-        for (Email checkedUser : EmailApp.usersList) {
+        for (Email checkedUser : usersList) {
             if (checkedUser.getFirstName().equals(firstName)
                     && checkedUser.getLastName().equals(lastName)) {
                 return checkedUser;
             }
         }
-
         return null;
     }
 
-    // Main process of the program.
-    public static void mainMethod() {
-        boolean quit = false;
-        Email user;
-        while (!quit) {
-
-            menuScreen();
-
-            int choice = input.nextInt();
-            input.nextLine();
-
-            switch (choice) {
-                case 0: // Exit the program.
-                    System.out.println("Exiting Application now.\nSee you again soon.");
-                    quit = true;
-                    break;
-
-                case 1: // Create new customer
-                    System.out.println("Enter the first name of the user:\r");
-                    String firstName = input.nextLine();
-                    System.out.println("Enter the last name of the user:\r");
-                    String lastName = input.nextLine();
-
-                    // fixing mistypes by users
-                    firstName = firstName.replaceAll("\\s+", "");
-                    lastName = lastName.replaceAll("\\s+", "");
-
-                    if (findUser(firstName, lastName) == null) {
-                        EmailApp.usersList.add(new Email(firstName, lastName));
-                    } else {
-                        System.out.println("The user already exists in the system.\n" +
-                                "Creating new email account is canceled.");
-                    }
-                    break;
-
-                case 2: // Get email
-                    user = findUser();
-                    if (user != null) {
-                        System.out.println("The email of " + user.getFirstName() + " "
-                                + user.getLastName() + " is:\n" + user.getEmail());
-                    } else {
-                        System.out.println("There is no user with this name.");
-                    }
-                    break;
-
-                case 3: // Get users password
-                    user = findUser();
-                    if (user != null) {
-                        System.out.println("The password of " + user.getFirstName() + " "
-                                + user.getLastName() + " is:\n" + user.getPassword());
-                    } else {
-                        System.out.println("There is no user with this name.");
-                    }
-                    break;
-
-                case 4: // Password change by the user.
-                    user = findUser();
-                    if (user != null) {
-                        System.out.println("Please enter the old password:\n");
-                        String oldPassword = input.nextLine();
-                        System.out.println("Please enter the new password:\n");
-                        String newPassword = input.nextLine();
-                        user.changePassword(oldPassword, newPassword);
-                    } else {
-                        System.out.println("There is no user with this name.");
-                    }
-                    break;
-
-                case 5: // Issue new password.
-                    user = findUser();
-                    if (user != null) {
-                        user.setPassword(user.getDefaultPasswordLength());
-                        System.out.println(user.getEmail() + " new password is: "
-                                + user.setPassword(user.getDefaultPasswordLength()));
-                    } else {
-                        System.out.println("There is no user with this name.");
-                    }
-                    break;
-
-                case 6: // Get mailbox capacity.
-                    user = findUser();
-                    if (user != null) {
-                        System.out.println(" User with email: " + user.getEmail() +
-                                "\nHas a  mailbox capacity of : " + user.getMailboxCapacity() + " Mb.");
-                    } else {
-                        System.out.println("There is no user with this name.");
-                    }
-                    break;
-
-                case 7: // Change mailbox capacity.
-                    user = findUser();
-                    if (user != null) {
-                        System.out.println("Enter new mailbox capacity for " + user.getEmail());
-                        int newCapacity = input.nextInt();
-                        input.nextLine();
-                        user.setMailboxCapacity(newCapacity);
-                    } else {
-                        System.out.println("There is no user with this name.");
-                    }
-                    break;
-
-                case 8: // Declare alternate email.
-                    user = findUser();
-                    if (user != null) {
-                        System.out.println("Provide me the alternate email of the user: " + user.getEmail());
-                        String alternateEmail = input.nextLine();
-                        alternateEmail = alternateEmail.replaceAll("\\s+", "");
-                        user.setAlternateEmail(alternateEmail);
-                        System.out.println("New alternate email is saved.");
-                    } else {
-                        System.out.println("There is no user with this name.");
-                    }
-                    break;
-
-                case 9:// print email accounts.
-
-                    for (Email email : EmailApp.usersList) {
-                        System.out.println("User name: " + email.getFirstName() + " "
-                                + email.getLastName() + "\nEmail address: " + email.getEmail()
-                                + "\n------------------------------------------------------------------");
-                    }
-                    break;
-            }
-        }
-    }
 }
 
 
--- src/emailApp/com/georgioskachrimanis/EmailApp.java
+++ src/emailApp/com/georgioskachrimanis/EmailApp.java
@@ -6,16 +6,60 @@ I would love to hear your comment/criticism on GitHub.
 * */
 package emailApp.com.georgioskachrimanis;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 public class EmailApp {
 
-    public static List<Email> usersList;
+    private static final Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        usersList= new ArrayList<>();
+        boolean quit = false;
 
-        Email.mainMethod();
+        while (!quit) {
+
+            Email.optionsMenu();
+
+            int choice = input.nextInt();
+            input.nextLine();
+
+            switch (choice) {
+                case 0: // Exit the program.
+                    System.out.println("Exiting Application now.\nSee you again soon.");
+                    quit = true;
+                    break;
+
+                case 1: // Create new email account
+                    Email.createNewUser();
+                    break;
+
+                case 2: // Show Users email
+                    Email.printEmail();
+                    break;
+
+                case 3: // Password change by the user.
+                    Email.userChangePassword();
+                    break;
+
+                case 4: // Issue new password.
+                    Email.issueNewPassword();
+                    break;
+
+                case 5: // Get mailbox capacity.
+                   Email.mailboxCapacity();
+                    break;
+
+                case 6: // Change mailbox capacity.
+                    Email.changeMailboxCapacity();
+                    break;
+
+                case 7: // Declare alternate email.
+                    Email.alternateMailbox();
+                    break;
+
+                case 8:// print email accounts.
+                    Email.printEmailAccounts();
+                    break;
+            }
+        }
    }
 }
